6aef8ec38b9bc644861fa4c2bb162c4c182c3bbe
jujopico <sujunqt@gmail.com>
2019-11-17 20:32:19 -0500

    add index action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
 
   def show
     @article = Article.find(params[:id])
@@ -18,6 +21,5 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :text)
   end
-
 end
 
