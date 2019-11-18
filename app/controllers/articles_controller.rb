4639afe0702d659ef9611695c5355761a58471cd
jujopico <sujunqt@gmail.com>
2019-11-17 20:24:56 -0500

    add show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
@@ -13,4 +18,6 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :text)
   end
+
 end
+
